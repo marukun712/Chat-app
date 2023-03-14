e2fb13b2dbb8764d5f4db75594151e38b293589c
marukun712 <marukun_@DESKTOP-PPH29DN.localdomain>
2023-03-14 15:04:39 +0900

    :sparkles: OGPを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useUser } from '@supabase/auth-helpers-react'
 import LoginForm from '@/components/LoginForm';
 import Message from '@/components/Message';
 import Post from '@/components/Post';
+import Head from 'next/head';
 
 export default function Home(): JSX.Element {
   const user = useUser()
@@ -9,11 +10,29 @@ export default function Home(): JSX.Element {
   //認証していなければログインフォームを返す
   if (!user)
     return (
-      <LoginForm />
+      <div>
+        <Head>
+          <title>Chat-app:Login</title>
+          <meta property="og:title" content="Chat-app:Login" />
+          <meta property="og:description" content="Supabaseを使って作られた簡単なチャットアプリ。" />
+          <meta property="og:url" content="https://marukun712-chat-app.vercel.app/" />
+          <meta property="og:image" content="https://avatars.githubusercontent.com/u/54469796?s=280&v=4" />
+          <meta property="og:title" content="Chat-app" />
+        </Head>
+        <LoginForm />
+      </div>
     )
 
   return (
     <div>
+      <Head>
+        <title>Chat-app</title>
+        <meta property="og:title" content="Chat-app" />
+        <meta property="og:description" content="Supabaseを使って作られた簡単なチャットアプリ。" />
+        <meta property="og:url" content="https://marukun712-chat-app.vercel.app/" />
+        <meta property="og:image" content="https://avatars.githubusercontent.com/u/54469796?s=280&v=4" />
+        <meta property="og:title" content="Chat-app" />
+      </Head>
       <Message />
       <Post />
     </div>
